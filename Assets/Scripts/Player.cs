f56299413ad9d987903712842cd80fbdc3cc3c19
Original-Kimchi <39785268+Original-Kimchi@users.noreply.github.com>
2020-06-03 15:15:38 +0900

    UI
    - 남은시간,랭킹, 점수 등
    - 변경가능

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int score;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (CompareTag("Food"))
+        {
+            
+        }
+    }
+
+    public void PointUp(int _score)
+    {
+        score += _score;
+    }
+}
