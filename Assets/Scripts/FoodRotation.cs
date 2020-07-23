43a1be14a6a7481de4f08f746481e90017e11e8a
Jinsab <kimjinou116@naver.com>
2020-07-23 09:49:08 +0900

    회전 구현

---
+++ Assets/Scripts/FoodRotation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodRotation : MonoBehaviour
+{
+	[Header("회전 방향")]
+	public bool x;
+	public bool y = true;
+	public bool z;
+
+	[Header("회전 시간")]
+	public float timeX = 3f;
+	public float timeY = 3f;
+	public float timeZ = 3f;
+
+	private float rotationX = 360f;
+	private float rotationY = 360f;
+	private float rotationZ = 360f;
+
+	private float angleX;
+	private float angleY;
+	private float angleZ;
+
+	private void Update()
+	{
+		if (x) angleX += (rotationX / timeX) * Time.deltaTime;
+		if (y) angleY += (rotationY / timeY) * Time.deltaTime;
+		if (z) angleZ += (rotationZ / timeZ) * Time.deltaTime;
+
+		transform.localRotation = Quaternion.Euler(angleX, angleY, angleZ);
+	}
+}
