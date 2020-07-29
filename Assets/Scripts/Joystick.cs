9acb0e7fddc02089375573188628f20fe5656a43
Jinsab <kimjinou116@naver.com>
2020-07-29 11:47:02 +0900

    조이스틱 + 모션 테스트

--- Assets/Scripts/Joystick.cs
+++ Assets/Scripts/Joystick.cs
@@ -8,10 +8,11 @@ public class Joystick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPoint
 	public RectTransform rectJoyback;
 	public RectTransform rectJoystick;
 	public float m_fSpeed = 5.0f;
+	public float m_fSqr = 0f;
 	public GameObject player;
 
 	private float m_fRadius;
-	private float m_fSqr = 0f;
+	
 
 	private Vector3 m_vecMove;
 	private Vector2 m_vecNormal;
@@ -38,11 +39,11 @@ public class Joystick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPoint
 		vec = Vector2.ClampMagnitude(vec, m_fRadius);
 		rectJoystick.localPosition = vec;
 
-		float fSqr = (rectJoyback.position - rectJoystick.position).sqrMagnitude / (m_fRadius * m_fRadius);
+		m_fSqr = (rectJoyback.position - rectJoystick.position).sqrMagnitude / (m_fRadius * m_fRadius);
 
 		Vector2 vecNormal = vec.normalized;
 
-		m_vecMove = new Vector3(vecNormal.x * m_fSpeed * Time.deltaTime * fSqr, 0f, vecNormal.y * m_fSpeed * Time.deltaTime * fSqr);
+		m_vecMove = new Vector3(vecNormal.x * m_fSpeed * Time.deltaTime * m_fSqr, 0f, vecNormal.y * m_fSpeed * Time.deltaTime * m_fSqr);
 		player.transform.eulerAngles = new Vector3(0f, Mathf.Atan2(vecNormal.x, vecNormal.y) * Mathf.Rad2Deg, 0f);
 	}
 
---
+++ Assets/Scripts/PlayerMotion.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotion : MonoBehaviour
+{
+	public Joystick fSqr;
+	private Animator animator;
+
+    void Start()
+    {
+		animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Debug.Log(fSqr.m_fSqr);
+
+		if (fSqr.m_fSqr < 0.33f)
+		{
+			animator.SetInteger("animation", 0);
+		}
+		else if (fSqr.m_fSqr < 0.66f)
+		{
+			animator.SetInteger("animation", 1);
+		}
+		else
+		{
+			animator.SetInteger("animation", 2);
+		}
+    }
+}
