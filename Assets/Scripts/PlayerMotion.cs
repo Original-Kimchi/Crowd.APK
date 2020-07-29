6b02add2be45cca5c21fd4e5a2a6f7da769d3a40
Jinsab <kimjinou116@naver.com>
2020-07-29 11:57:45 +0900

    모션 체크

--- Assets/Scripts/PlayerMotion.cs
+++ Assets/Scripts/PlayerMotion.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMotion : MonoBehaviour
 {
 	public Joystick fSqr;
+	public Text text;
 	private Animator animator;
 
     void Start()
@@ -16,18 +18,21 @@ public class PlayerMotion : MonoBehaviour
     void Update()
     {
 		Debug.Log(fSqr.m_fSqr);
-
+		
 		if (fSqr.m_fSqr < 0.33f)
 		{
 			animator.SetInteger("animation", 0);
+			text.text = "Idle";
 		}
 		else if (fSqr.m_fSqr < 0.66f)
 		{
 			animator.SetInteger("animation", 1);
+			text.text = "Walk";
 		}
 		else
 		{
 			animator.SetInteger("animation", 2);
+			text.text = "Run";
 		}
     }
 }
