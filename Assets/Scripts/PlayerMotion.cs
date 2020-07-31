4c810db36be823ab362171ad9a284e4ab1078799
Original-Kimchi <39785268+Original-Kimchi@users.noreply.github.com>
2020-07-31 14:22:17 +0900

    Update PlayerMotion.cs
    Edit fsqr.m_fsqr size

--- Assets/Scripts/PlayerMotion.cs
+++ Assets/Scripts/PlayerMotion.cs
@@ -17,10 +17,10 @@ public class PlayerMotion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		Debug.Log(fSqr.m_fSqr);
+		Debug.Log("조이스틱" + fSqr.m_fSqr);
 		
-		if (fSqr.m_fSqr < 0.33f) animator.SetInteger("animation", 0);
-		else if (fSqr.m_fSqr < 0.66f) animator.SetInteger("animation", 1);
+		if (fSqr.m_fSqr < 0.1f) animator.SetInteger("animation", 0);
+		else if (fSqr.m_fSqr < 0.33f) animator.SetInteger("animation", 1);
 		else animator.SetInteger("animation", 2);
     }
 }
