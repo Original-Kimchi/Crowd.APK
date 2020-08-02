8c3e6c1710e9fd7300dd4cd3f3953da209d7fc27
김태현 <allone1129@gmail.com>
2020-08-02 14:56:17 +0900

    Implement PhotonViewObject script

---
+++ Assets/Scripts/PhotonViewObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public abstract class PhotonViewObject: MonoBehaviour
+{
+    public PhotonView PhotonView { get; private set; }
+
+    protected void Awake()
+    {
+        PhotonView = gameObject.GetComponent<PhotonView>();
+    }
+}
